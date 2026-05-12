bd3f8a152a068ebfa608c4ef3670c55751d1009b
whan1569 <48788071+whan1569@users.noreply.github.com>
2026-05-12 16:25:34 +0900

    Create types.ts

---
+++ frontend/app/products/features/types.ts
@@ -0,0 +1,31 @@
+export type ProductCategory =
+  | "전체"
+  | "스킨케어"
+  | "클렌징"
+  | "선케어"
+  | "마스크팩"
+  | "기타";
+
+export type ProductActiveCategory = Exclude<ProductCategory, "전체">;
+
+export type OriginCountry =
+  | "대한민국"
+  | "일본"
+  | "중국"
+  | "미국";
+
+export type Product = {
+  id: number;
+  productName: string;
+  sku: string;
+  category: ProductActiveCategory;
+  createdAt: string;
+};
+
+export type ProductFilter = {
+  productName: string;
+  sku: string;
+  category: ProductCategory;
+};
+
+export type ProductDrawerMode = "create" | "edit";
